e3a0d0ac5abb8cd230aaa5518bbc88e42dc6a57a
Alla Kutina <allakv3@gmail.com>
2019-09-21 13:08:44 +0300

    Доработка бота

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
+import ephem
+
+from datetime import datetime, date
+
 import logging
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -19,11 +23,27 @@ def talk_to_me(bot, update):
     print(user_text)
     update.message.reply_text(user_text)
 
+def get_constellation(bot, update):
+    user_text = update.message.text
+    planet = user_text.split(' ')[1].lower()
+    today = date.today()
+    now = '{}/{}/{}'.format(today.year, today.month, today.day)
+
+    if(planet == 'mars'):
+        mars = ephem.Mars(now)
+        const = ephem.constellation(mars)
+        result = ','.join(const)
+        update.message.reply_text(result)
+    else:
+        update.message.reply_text('Я не знаю такой планеты')
+
 def main():
     mybot = Updater('953569815:AAGuhJ4QyUdlc_75KLLMnrbieJ157TQtCCk', request_kwargs=PROXY)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", get_constellation))
+    dp.add_handler(MessageHandler(Filters.text, get_constellation))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
